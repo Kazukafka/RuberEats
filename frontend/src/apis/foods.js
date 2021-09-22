2a9b62e34ff63df5d9832c2d2de2e2ce37cc2a6e
Kazukafka <pasuwaado135@gmail.com>
2021-09-22 22:38:56 +0900

    APIdata through reducer

---
+++ frontend/src/apis/foods.js
@@ -0,0 +1,12 @@
+// --- ここから追加 ---
+import axios from 'axios';
+import { foodsIndex } from '../urls/index'
+
+export const fetchFoods = (restaurantId) => {
+  return axios.get(foodsIndex(restaurantId))
+    .then(res => {
+      return res.data
+    })
+    .catch((e) => console.error(e))
+}
+// --- ここまで追加 ---
--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,9 +1,49 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useReducer } from 'react';
 
-export const Foods = () => {
+import {
+  initialState as foodsInitialState,
+  foodsActionTyps,
+  foodsReducer,
+} from '../reducers/foods';
+
+import { fetchFoods } from '../apis/foods';
+
+import { REQUEST_STATE } from '../constants';
+
+export const Foods = ({
+  match
+}) => {
+  const [foodsState, dispatch] = useReducer(foodsReducer, foodsInitialState);
+
+  useEffect(() => {
+    dispatch({ type: foodsActionTyps.FETCHING });
+    fetchFoods(match.params.restaurantsId)
+      .then((data) => {
+        dispatch({
+          type: foodsActionTyps.FETCH_SUCCESS,
+          payload: {
+            foods: data.foods
+          }
+        });
+      })
+  }, [])
+
   return (
     <Fragment>
-      フード一覧
+      {
+        foodsState.fetchState === REQUEST_STATE.LOADING ?
+          <Fragment>
+            <p>
+              ロード中...
+            </p>
+          </Fragment>
+          :
+          foodsState.foodsList.map(food =>
+            <div key={food.id}>
+              {food.name}
+            </div>
+          )
+      }
     </Fragment>
   )
 }
